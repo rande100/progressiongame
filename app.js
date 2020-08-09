b2e97c4f68794afedc0f876eba31f265b61dd773
rande100 <69306025+rande100@users.noreply.github.com>
2020-08-10 00:44:24 +1000

    Daily Add weapons Add chance of weapon drop on mob death Add weapon damage to player base damage Add game log for xp, level up, loot

--- app.js
+++ app.js
@@ -4,20 +4,45 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/client/index.html");
+  res.sendFile(__dirname + "/client/index.html");
 });
 app.use("/client", express.static(__dirname + "/client"));
 
 http.listen(2000);
 console.log("Server started. Listening on port 2000.");
 
+class Weapon {
+  constructor() {
+    this.id = 0;
+    this.name = "";
+    this.attack = 0;
+  }
+}
+
+let weaponList = {};
+
+weaponList[0] = new Weapon();
+weaponList[0]["id"] = 0;
+weaponList[0]["name"] = "Wooden Club";
+weaponList[0]["attack"] = 1;
+
+weaponList[1] = new Weapon();
+weaponList[1]["id"] = 1;
+weaponList[1]["name"] = "Copper Short Sword";
+weaponList[1]["attack"] = 2;
+
+weaponList[2] = new Weapon();
+weaponList[2]["id"] = 2;
+weaponList[2]["name"] = "Iron Longsword";
+weaponList[2]["attack"] = 3;
+
 class Mob {
-	constructor() {
-		this.id = "";
-		this.name = "";
-		this.level = 0;
-		this.health = 0;
-	}
+  constructor() {
+    this.id = "";
+    this.name = "";
+    this.level = 0;
+    this.health = 0;
+  }
 }
 
 let mobList = {};
@@ -53,125 +78,174 @@ mobList["golem"]["health"] = mobList["golem"]["maxHealth"];
 let mob = {};
 
 class Player {
-	constructor() {
-		this.name = "Player";
-		this.level = 1;
-		this.health = 10;
-		this.maxHealth = 10;
-		this.xp = 0;
-	}
+  constructor() {
+    this.name = "Player";
+    this.level = 1;
+    this.health = 10;
+    this.maxHealth = 10;
+    this.xp = 0;
+    this.weapon = null;
+  }
 }
 
 // list of all connected players (key is socket ID, value is Player object)
 let playerList = {};
 
 io.on("connection", function (socket) {
-	console.log("User connected. ID: " + socket.id);
-	playerList[socket.id] = new Player();
-	console.log("Connected users: " + Object.keys(playerList).length);
+  console.log("User connected. ID: " + socket.id);
+  playerList[socket.id] = new Player();
+  console.log("Connected users: " + Object.keys(playerList).length);
 
-	// init
-	socket.emit("mob list update", mobList);
-	io.emit("mob update", mob);
-	io.emit("player list update", playerList);
+  // init
+  socket.emit("mob list update", mobList);
+  io.emit("mob update", mob);
+  io.emit("player list update", playerList);
 
-	socket.on("disconnect", () => {
-		console.log("User disconnected. ID: " + socket.id);
-		delete playerList[socket.id];
-		io.emit("player list update", playerList);
-	});
+  socket.on("disconnect", () => {
+    console.log("User disconnected. ID: " + socket.id);
+    delete playerList[socket.id];
+    io.emit("player list update", playerList);
+  });
 
-	socket.on("attack", () => {
-		if (mob.health <= 0) {
-			console.log("User attacked but mob is already dead. ID: " + socket.id);
-			return;
-		}
+  socket.on("attack", () => {
+    if (mob.health <= 0) {
+      console.log("User attacked but mob is already dead. ID: " + socket.id);
+      return;
+    }
 
-		console.log("User attacked. ID: " + socket.id);
+    console.log("User attacked. ID: " + socket.id);
 
-		let playerDamage = 1;
-		mob.health -= playerDamage;
+    let playerDamage = 1;
+    if (playerList[socket.id]["weapon"]) {
+      playerDamage += playerList[socket.id]["weapon"]["attack"];
+    }
+    mob.health -= playerDamage;
 
-		io.emit(
-			"gamelog",
-			playerList[socket.id]["name"] + " attacks " + mob.name + " for " + playerDamage + " damage."
-		);
+    io.emit(
+      "gamelog",
+      playerList[socket.id]["name"] +
+        " attacks " +
+        mob.name +
+        " for " +
+        playerDamage +
+        " damage."
+    );
 
-		// is the mob dead?
-		if (mob.health <= 0) {
-			mob.health = 0;
+    // is the mob dead?
+    if (mob.health <= 0) {
+      mob.health = 0;
 
-			io.emit("gamelog", mob.name + " dies.");
+      io.emit("gamelog", mob.name + " dies.");
 
-			// grant xp to all players
-			for (let key in playerList) {
-				playerList[key]["xp"] += 2 + mob.level;
+      // grant xp to all players
+      for (let key in playerList) {
+        let grantXP = 2 + mob.level;
+        playerList[key]["xp"] += grantXP;
 
-				// check player xp for level-up
-				if (playerList[key]["xp"] >= playerList[key]["level"] * 10) {
-					// increase player level and health
-					playerList[key]["level"]++;
-					playerList[key]["maxHealth"]++;
-					playerList[key]["health"] = playerList[key]["maxHealth"];
-				}
-			}
-		}
+        io.emit(
+          "gamelog",
+          playerList[key]["name"] + " gains " + grantXP + " experience."
+        );
 
-		// sync all players
-		io.emit("player list update", playerList);
+        // check player xp for level-up
+        if (playerList[key]["xp"] >= playerList[key]["level"] * 10) {
+          // increase player level and health
+          playerList[key]["level"]++;
+          playerList[key]["maxHealth"]++;
+          playerList[key]["health"] = playerList[key]["maxHealth"];
 
-		// sync mob
-		io.emit("mob update", mob);
-	});
+          io.emit("gamelog", playerList[key]["name"] + " has levelled up.");
+        }
+      }
 
-	socket.on("player name save", function (playerName) {
-		console.log("User changed name. ID: " + socket.id);
-		playerList[socket.id]["name"] = playerName;
-		io.emit("player list update", playerList);
-	});
+      // each player has a chance for loot individually
+      for (let key in playerList) {
+        // drop a weapon?
+        if (Math.random() < 0.5) {
+          let weaponDropped = weaponList[0];
+          playerList[key]["weapon"] = Object.assign({}, weaponDropped);
+          io.emit(
+            "gamelog",
+            "<strong>" +
+              playerList[key]["name"] +
+              " receives a " +
+              weaponDropped["name"] +
+              "!</strong>"
+          );
+        }
+      }
+    }
 
-	socket.on("mob spawn", function (mobName) {
-		console.log("User spawned mob. ID: " + socket.id);
+    // sync all players
+    io.emit("player list update", playerList);
 
-		mob = Object.assign({}, mobList[mobName]);
+    // sync mob
+    io.emit("mob update", mob);
+  });
 
-		io.emit("mob update", mob);
-	});
+  socket.on("player name save", function (playerName) {
+    console.log("User changed name. ID: " + socket.id);
+    playerList[socket.id]["name"] = playerName;
+    io.emit("player list update", playerList);
+  });
+
+  socket.on("mob spawn", function (mobName) {
+    console.log("User spawned mob. ID: " + socket.id);
+
+    mob = Object.assign({}, mobList[mobName]);
+
+    io.emit("mob update", mob);
+  });
 });
 
 function mobAttack() {
-	// is mob alive?
-	if (mob.health > 0) {
-		let numPlayers = Object.keys(playerList).length;
-		let targetPlayerNum = Math.floor(Math.random() * numPlayers);
-		let targetPlayerID = null;
-		let mobDamage = 0;
-		let i = 0;
-		for (let key in playerList) {
-			if (i === targetPlayerNum) {
-				targetPlayerID = key;
+  // is mob alive?
+  if (mob.health > 0) {
+    let numPlayers = Object.keys(playerList).length;
+    let targetPlayerNum = Math.floor(Math.random() * numPlayers);
+    let targetPlayerID = null;
+    let mobDamage = 0;
+    let i = 0;
+    for (let key in playerList) {
+      if (i === targetPlayerNum) {
+        targetPlayerID = key;
 
-				// is target player dead?
-				if (playerList[key]["health"] <= 0) {
-					break;
-				}
+        // is target player dead?
+        if (playerList[key]["health"] <= 0) {
+          break;
+        }
 
-				// attack target player
-				mobDamage = mob.level;
-				playerList[key]["health"] -= mobDamage;
-				if (playerList[key]["health"] < 0) playerList[key]["health"] = 0;
+        // attack target player
+        mobDamage = mob.level;
+        playerList[key]["health"] -= mobDamage;
+        if (playerList[key]["health"] < 0) playerList[key]["health"] = 0;
 
-				console.log("Mob attacks " + targetPlayerID + " for " + mobDamage + " damage.");
-				io.emit("gamelog", mob.name + " attacks " + playerList[key]["name"] + " for " + mobDamage + " damage.");
+        console.log(
+          "Mob attacks " + targetPlayerID + " for " + mobDamage + " damage."
+        );
+        io.emit(
+          "gamelog",
+          mob.name +
+            " attacks " +
+            playerList[key]["name"] +
+            " for " +
+            mobDamage +
+            " damage."
+        );
 
-				// sync all players
-				io.emit("player list update", playerList);
+        // did the target player die?
+        if (playerList[key]["health"] === 0) {
+          io.emit("gamelog", playerList[key]["name"] + " dies.");
+        }
 
-				break;
-			}
-			i++;
-		}
-	}
+        // sync all players
+        io.emit("player list update", playerList);
+
+        break;
+      }
+      i++;
+    }
+  }
 }
 
 setInterval(mobAttack, 3000);
